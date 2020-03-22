123e801e448e2cce6670fb6093d4e1c920e99c97
Smite Chow <xiaopengyou2no1@gmail.com>
2020-03-22 18:27:28 +0800

    add basic auth for generic proxy python

--- generic.py
+++ generic.py
@@ -1,12 +1,31 @@
+import base64
 import http.server
 import socketserver
+from http import HTTPStatus
 
 import requests
 
 PORT = 8888
 
 class Handler(http.server.BaseHTTPRequestHandler):
+    def return_407(self):
+        self.send_response(HTTPStatus.PROXY_AUTHENTICATION_REQUIRED)
+        self.send_header('Proxy-Authenticate', 'Basic realm="demo proxy"')
+        self.end_headers()
+
+    def validate(self, credential):
+        try:
+            credential = credential.split(' ')[1]
+            credential = base64.b64decode(credential.encode()).decode()
+            username, password = credential.split(':')
+            return username == 'test' and password == 'testme'
+        except:
+            return False
+
     def do_GET(self):
+        credential = self.headers.get('Proxy-Authorization')
+        if credential is None or not self.validate(credential):
+            self.return_407()
         print(f'proxy for:{self.path}')
         response = requests.get(self.path, stream=True)
         for chunk in response.iter_content(chunk_size=128):
